e64350a4663958fb6f1460c19c17c221e370e873
Jens Ochsenmeier <github@jens-ox.de>
2022-07-19 20:16:05 +0200

    walk through github pagination

--- refresh.js
+++ refresh.js
@@ -3,30 +3,43 @@ const yaml = require('js-yaml')
 const fs = require('fs/promises')
 
 const main = async () => {
-  // fetch repo list
-  const { data } = await axios.get('https://api.github.com/users/BundesAPI/repos')
+  let empty = false
+  let page = 1
+  let entries = []
 
-  // only check repos that end in "-api"
-  const repos = data.filter((repo) => repo.name.endsWith('-api'))
+  while (!empty) {
+    // fetch repo list
+    const { data } = await axios.get(`https://api.github.com/users/BundesAPI/repos?page=${page}`)
 
-  const result = await Promise.all(repos.map(async (repo) => {
-    const { data: rawRepoData } = await axios.get(`https://raw.githubusercontent.com/bundesAPI/${repo.name}/main/openapi.yaml`)
-    const repoData = yaml.load(rawRepoData)
+    // break on empty list
+    if (data.length === 0) empty = true
+  
+    // only check repos that end in "-api"
+    const repos = data.filter((repo) => repo.name.endsWith('-api'))
+  
+    const result = await Promise.all(repos.map(async (repo) => {
+      const { data: rawRepoData } = await axios.get(`https://raw.githubusercontent.com/bundesAPI/${repo.name}/main/openapi.yaml`)
+      const repoData = yaml.load(rawRepoData)
+  
+      return {
+        name: repoData.info.title,
+        office: repoData.info['x-office'],
+        description: repo.description,
+        documentationURL: repo.homepage,
+        githubURL: repo.html_url
+      }
+    }))
 
-    return {
-      name: repoData.info.title,
-      office: repoData.info['x-office'],
-      description: repo.description,
-      documentationURL: repo.homepage,
-      githubURL: repo.html_url
-    }
-  }))
+    entries.push(...result)
+    page += 1
+  }
+
 
   const overridesRaw = await fs.readFile('./overrides.json', { encoding: 'utf-8' })
   const overrides = await JSON.parse(overridesRaw)
 
   // upsert list
-  const newList = result.map((repo) => {
+  const newList = entries.map((repo) => {
     const overrideIndex = overrides.findIndex((e) => e.githubURL === repo.githubURL)
     if (overrideIndex === -1) return repo
 
